453de67c4689e3974f1fe04739d5a48b1702a6cf
TAN <thanishka.k06@gmail.com>
2025-12-20 22:10:27 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// use of stranger-things commonjs module
-const strangerThings = require('stranger-things');
-var name=strangerThings.random();
-console.log("Random Stranger Things Character: "+name);
-
-// use of superheros module
-// import superheros from "superheros";
-// var name1 = superheros.random();
-// console.log("I like playing "+name1);
