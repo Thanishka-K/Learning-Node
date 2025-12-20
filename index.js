0463729f7123f3baae6d0675e3944057ffd9cbd1
TAN <thanishka.k06@gmail.com>
2025-12-20 22:07:50 +0530

    learning installation using npm and nodemon

---
+++ index.js
@@ -0,0 +1,9 @@
+// use of stranger-things commonjs module
+const strangerThings = require('stranger-things');
+var name=strangerThings.random();
+console.log("Random Stranger Things Character: "+name);
+
+// use of superheros module
+// import superheros from "superheros";
+// var name1 = superheros.random();
+// console.log("I like playing "+name1);
