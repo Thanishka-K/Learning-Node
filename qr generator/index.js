303942f77afa1f1937af57258265cb61d705ab9b
TAN <thanishka.k06@gmail.com>
2025-12-23 20:46:20 +0530

    comments
    Updated comments for clarity and added a missing newline at the end of the file.

--- qr generator/index.js
+++ qr generator/index.js
@@ -10,19 +10,20 @@ inquirer
   }
   ])
   .then((answers) => {
-    const url=answers.URL;
-    var qr_svg = qr.image(url); //to create qr code
+    const url=answers.URL;//to save the url
+    var qr_svg = qr.image(url); //to create qr code using .image(url)
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
  
     writeFile("URL.txt",url,(err)=>{
-       if (err) throw err;
+       if (err) throw err;//to throw the following statement if there a error
     console.log("QR code generated successfully!");
     })
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
+      // Prompt couldn't be rendered in the current environment //just wrote it simply
     } else {
       // Something else went wrong
     }
+
   });
